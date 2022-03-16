b806f246e0099388383d758645083e280d66eff1
Jude <lovespictures2@gmail.com>
2022-03-16 17:27:04 +0000

    Update ex 3

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,16 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+let sum = 0;
+console.log(`QTY\t` + `ITEM`.padEnd(20, " ") + `TOTAL`);
+order.map((item) => {
+  let { quantity, itemName, unitPrice } = item;
+
+  let total = quantity * unitPrice;
+
+  sum += total;
+  console.log(quantity + `\t` + itemName.padEnd(20, " "), total.toFixed(2));
+});
+
+console.log(`Total: ` + sum);
